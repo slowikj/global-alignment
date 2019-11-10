3e9e389093e34fd3d840e017267ad2b40e88508b
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-10 14:16:15 +0100

    NeedlemanWunschSolver: add diff_cost field

--- src/needleman_wunsch_solver.py
+++ src/needleman_wunsch_solver.py
@@ -1,5 +1,6 @@
 class NeedlemanWunschSolver:
 
-    def __init__(self, gap_penalty: int, same_cost: int):
+    def __init__(self, gap_penalty: int, same_cost: int, diff_cost: int):
         self.gap_penalty = gap_penalty
         self.same_cost = same_cost
+        self.diff_cost = diff_cost
