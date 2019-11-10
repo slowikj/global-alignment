a311bbb122cacbf7058f6600702f33b0fedf5cda
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-10 14:11:12 +0100

    NeedlemanWunschTests: add same_cost field requirement

--- tests/needleman_wunsch_tests.py
+++ tests/needleman_wunsch_tests.py
@@ -7,8 +7,14 @@ class NeedlemanWunschTests(unittest.TestCase):
 
     gap_penalty = -2
 
+    same_cost = 5
+
     def setUp(self):
         self.solver = NeedlemanWunschSolver(gap_penalty=self.gap_penalty)
 
     def test_solver_has_properly_initialized_gap_penalty_field(self):
         self.assertEqual(self.solver.gap_penalty, self.gap_penalty)
+
+    def test_solver_has_properly_initialized_same_cost_field(self):
+        self.assertEqual(self.solver.same_cost, self.same_cost)
+
