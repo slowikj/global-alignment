841de5ba52f000bb540c4cef9b6e4804d4ecee21
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-10 16:36:34 +0100

    NeedlemanWunschSolver: add cost matrix computation

--- src/needleman_wunsch_solver.py
+++ src/needleman_wunsch_solver.py
@@ -23,6 +23,22 @@ class NeedlemanWunschSolver(object):
         else:
             return 0
 
+    def compute_cost_matrix(self, a_seq: str, b_seq: str):
+        len_a, len_b = len(a_seq), len(b_seq)
+        cost_matrix = self.generate_initial_cost_matrix(a_seq_len=len_a, b_seq_len=len_b)
+
+        for r in range(1, len(cost_matrix)):
+            for c in range(1, len(cost_matrix[0])):
+                cost_matrix[r][c] = max(
+                    cost_matrix[r][c - 1] + self.gap_penalty,
+                    cost_matrix[r-1][c-1] + self.__item_comparison_cost(a_seq[r - 1], b_seq[c - 1]),
+                    cost_matrix[r - 1][c] + self.gap_penalty)
+
+        return cost_matrix
+
+    def __item_comparison_cost(self, a, b):
+        return self.same_cost if a == b else self.diff_cost
+
     @staticmethod
     def __generate_matrix(height: int, width: int, cell_generator):
         return [[cell_generator(r, c) for c in range(width)]
