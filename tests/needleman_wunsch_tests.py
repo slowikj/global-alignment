46d87505cedb5ea60651d63c9f41b220f0094e76
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-10 14:15:29 +0100

    NeedlemanWunschTests: add diff_cost property requirement

--- tests/needleman_wunsch_tests.py
+++ tests/needleman_wunsch_tests.py
@@ -9,14 +9,20 @@ class NeedlemanWunschTests(unittest.TestCase):
 
     same_cost = 5
 
+    diff_cost = -5
+
     def setUp(self):
         self.solver = NeedlemanWunschSolver(
             gap_penalty=self.gap_penalty,
-            same_cost=self.same_cost)
+            same_cost=self.same_cost,
+            diff_cost=self.diff_cost)
 
     def test_solver_has_properly_initialized_gap_penalty_field(self):
         self.assertEqual(self.solver.gap_penalty, self.gap_penalty)
 
     def test_solver_has_properly_initialized_same_cost_field(self):
         self.assertEqual(self.solver.same_cost, self.same_cost)
+
+    def test_solver_has_properly_initialized_diff_cost_field(self):
+        self.assertEqual(self.solver.diff_cost, self.diff_cost)
 
