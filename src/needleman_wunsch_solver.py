ca20a94996bfc08627f254593e6c8de77609d388
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-10 17:03:43 +0100

    NeedlemanWunschSolver: refactor: move cell cost computation to a new method

--- src/needleman_wunsch_solver.py
+++ src/needleman_wunsch_solver.py
@@ -5,6 +5,18 @@ class NeedlemanWunschSolver(object):
         self.same_cost = same_cost
         self.diff_cost = diff_cost
 
+    def compute_cost_matrix(self, a_seq: str, b_seq: str):
+        len_a, len_b = len(a_seq), len(b_seq)
+        cost_matrix = self.generate_initial_cost_matrix(
+            a_seq_len=len_a,
+            b_seq_len=len_b)
+
+        for r in range(1, len(cost_matrix)):
+            for c in range(1, len(cost_matrix[0])):
+                self.__compute_cost_matrix_cell(cost_matrix, a_seq, b_seq, r, c)
+
+        return cost_matrix
+
     def generate_initial_cost_matrix(self, a_seq_len: int, b_seq_len: int):
         return self.__generate_matrix(
             height=a_seq_len + 1,
@@ -17,31 +29,24 @@ class NeedlemanWunschSolver(object):
             width=b_seq_len + 1,
             cell_generator=lambda r, c: [])
 
-    def __initial_cost_matrix_cell_generator(self, row: int, col: int):
-        if row == 0 or col == 0:
-            return max(row, col) * self.gap_penalty
-        else:
-            return 0
-
-    def compute_cost_matrix(self, a_seq: str, b_seq: str):
-        len_a, len_b = len(a_seq), len(b_seq)
-        cost_matrix = self.generate_initial_cost_matrix(a_seq_len=len_a, b_seq_len=len_b)
-
-        for r in range(1, len(cost_matrix)):
-            for c in range(1, len(cost_matrix[0])):
-                cost_matrix[r][c] = max(
-                    cost_matrix[r][c - 1] + self.gap_penalty,
-                    cost_matrix[r-1][c-1] + self.__item_comparison_cost(a_seq[r - 1], b_seq[c - 1]),
-                    cost_matrix[r - 1][c] + self.gap_penalty)
-
-        return cost_matrix
+    def __compute_cost_matrix_cell(self, cost_matrix, a_seq, b_seq, r, c):
+        cost_matrix[r][c] = max(
+            cost_matrix[r][c - 1] + self.gap_penalty,
+            cost_matrix[r - 1][c - 1] + self.__item_comparison_cost(a_seq[r - 1], b_seq[c - 1]),
+            cost_matrix[r - 1][c] + self.gap_penalty)
 
     def __item_comparison_cost(self, a, b):
         return self.same_cost if a == b else self.diff_cost
 
     @staticmethod
     def __generate_matrix(height: int, width: int, cell_generator):
-        return [[cell_generator(r, c) for c in range(width)]
-                for r in range(height)
-                ]
+        return [
+            [cell_generator(r, c) for c in range(width)]
+            for r in range(height)
+        ]
 
+    def __initial_cost_matrix_cell_generator(self, row: int, col: int):
+        if row == 0 or col == 0:
+            return max(row, col) * self.gap_penalty
+        else:
+            return 0
