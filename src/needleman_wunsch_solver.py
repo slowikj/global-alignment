9b806fad137c74db5867f97c18b2809ef8887210
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-10 15:16:53 +0100

    NeedlemanWunschSolver: refactor: move the initial matrix generation to a common method

--- src/needleman_wunsch_solver.py
+++ src/needleman_wunsch_solver.py
@@ -1,4 +1,4 @@
-class NeedlemanWunschSolver:
+class NeedlemanWunschSolver(object):
 
     def __init__(self, gap_penalty: int, same_cost: int, diff_cost: int):
         self.gap_penalty = gap_penalty
@@ -6,9 +6,11 @@ class NeedlemanWunschSolver:
         self.diff_cost = diff_cost
 
     def generate_initial_cost_matrix(self, a_seq_len: int, b_seq_len: int):
-        res = [
-            [0 for _ in range(a_seq_len + 1)] for _ in range(b_seq_len + 1)
-        ]
+        res = self.__generate_matix(
+            height=b_seq_len + 1,
+            width=a_seq_len + 1,
+            cell_generator=lambda r, c: 0
+        )
 
         for i in range(a_seq_len + 1):
             res[0][i] = i * self.gap_penalty
@@ -18,6 +20,14 @@ class NeedlemanWunschSolver:
 
         return res
 
-    def generate_initial_directions_matrix(self, a_seq_len, b_seq_len):
-        return [[] for _ in range(a_seq_len + 1)] * (b_seq_len + 1)
+    def generate_initial_directions_matrix(self, a_seq_len: int, b_seq_len: int):
+        return self.__generate_matix(
+            height=b_seq_len + 1,
+            width=a_seq_len + 1,
+            cell_generator=lambda r, c: [])
+
+    def __generate_matix(self, height: int, width: int, cell_generator):
+        return [[cell_generator(r, c) for c in range(width)]
+                for r in range(height)
+                ]
 
