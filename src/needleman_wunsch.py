4a43af34eb0c6a6f421e8019d5d44777f678ffa3
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-11 16:37:49 +0100

    Read params from file and write the output to a file

--- src/needleman_wunsch.py
+++ src/needleman_wunsch.py
@@ -1,4 +1,9 @@
 from typing import Dict, Tuple, Set
+import sys
+import getopt
+
+from src.config_analysis import IntConfigReader
+from src.solver import NeedlemanWunschSolver, CellCostComputer
 
 
 class NeedlemanWunschParamsValidator(object):
@@ -24,3 +29,82 @@ class NeedlemanWunschParamsValidator(object):
     def validate_sequence(sequence: str, max_sequence_length: int) -> bool:
         return len(sequence) <= max_sequence_length
 
+
+def read_sequence_from_fasta(file_path: str) -> str:
+    with open(file_path, "r") as f:
+        res = ""
+        f.readline()  # we are not interested in the first line
+        for line in f:
+            res += line
+        return res
+
+
+def read_config(file_path: str) -> Dict[str, int]:
+    with open(file_path, "r") as f:
+        config_reader = IntConfigReader()
+        for line in f:
+            config_reader.add_line(line)
+        return config_reader.get_attributes()
+
+
+def extract_config(file_path: str) -> Dict[str, int]:
+    try:
+        res = read_config(file_path)
+    except ValueError:
+        print("config file is invalid. Line format should be: KEY INT_VALUE")
+        sys.exit(2)
+    config_validation_status, missing_params, redundant_params = NeedlemanWunschParamsValidator() \
+        .validate_config(res)
+    if not config_validation_status:
+        print("config file is invalid: \n")
+        print("missing params: {}".format(missing_params))
+        print("redundant params: {}".format(redundant_params))
+        sys.exit(2)
+    return res
+
+
+def extract_params(options):
+    for opt, value in options:
+        if opt == '-a':
+            a = read_sequence_from_fasta(value)
+        elif opt == "-b":
+            b = read_sequence_from_fasta(value)
+        elif opt == "-c":
+            conf = extract_config(value)
+        elif opt == "-o":
+            out_path = value
+    return a, b, conf, out_path
+
+
+def get_alignments(config: Dict[str, int], seq_a: str, seq_b: str):
+    solver = NeedlemanWunschSolver(CellCostComputer(
+        same_cost=config["SAME"],
+        diff_cost=config["DIFF"],
+        gap_penalty=config["GAP_PENALTY"]
+    ))
+    return solver.generate_alignments(seq_a, seq_b, config["MAX_NUMBER_PATHS"])
+
+
+def compute_and_save_alignments(config: Dict[str, int], seq_a: str, seq_b: str, output_path: str):
+    score, alignments = get_alignments(config, seq_a, seq_b)
+    with open(output_path, "w") as output:
+        output.write("SCORE = {}\n\n".format(score))
+        for alignment in alignments:
+            output.write("{}\n{}\n\n".format(alignment[0], alignment[1]))
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    try:
+        opts, _ = getopt.getopt(args, "a:b:c:o:")
+    except getopt.GetoptError:
+        print("usage: python needleman_wunsch.py -a seq1.txt -b seq2.txt -c config.txt -o output.txt")
+        sys.exit(2)
+
+    seq_a, seq_b, config, output_path = extract_params(opts)
+
+    if all(map(lambda seq: NeedlemanWunschParamsValidator().validate_sequence(seq, config["MAX_SEQ_LENGTH"]),
+               [seq_a, seq_b])):
+        compute_and_save_alignments(config, seq_a, seq_b, output_path)
+    else:
+        print("at least one sequence is too long")
