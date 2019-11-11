4a05509658c2b171c3cebddfc17b6d0bbc7b2ab0
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-11 13:46:03 +0100

    config_reader: implement add_line without validation

--- src/config_analysis.py
+++ src/config_analysis.py
@@ -1,10 +1,14 @@
 from typing import Tuple, List
 
 
-class ConfigReader(object):
+class IntConfigReader(object):
+
+    def __init__(self):
+        self.__config_attrs: List[Tuple[str, object]] = []
 
     def get_attributes(self) -> List[Tuple[str, object]]:
-        return []
+        return self.__config_attrs
 
-    def add_line(self, line):
-        pass
+    def add_line(self, line: str):
+        attr_key, attr_value = line.split(sep=" ", maxsplit=2)
+        self.__config_attrs.append((attr_key, int(attr_value)))
--- tests/config_analysis_tests.py
+++ tests/config_analysis_tests.py
@@ -1,12 +1,12 @@
 import unittest
 
-from src.config_analysis import ConfigReader
+from src.config_analysis import IntConfigReader
 
 
 class ConfigAnalysisTests(unittest.TestCase):
 
     def setUp(self):
-        self.config_reader = ConfigReader()
+        self.config_reader = IntConfigReader()
 
     def test_empty_config_reader_returns_empty_list(self):
         self.assertListEqual(
@@ -22,7 +22,7 @@ class ConfigAnalysisTests(unittest.TestCase):
         # assert
         self.assertListEqual(
             self.config_reader.get_attributes(),
-            [("FIRST PARAM", 5)]
+            [("FIST_PARAM", 5)]
         )
 
 
