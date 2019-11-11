a29fe817d60e5cd2787baa8fd733f0089a4c7ed2
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-11 13:28:05 +0100

    config_reader: make empty state test pass

--- src/config_analysis.py
+++ src/config_analysis.py
@@ -1,3 +1,7 @@
+from typing import Tuple, List
+
+
 class ConfigReader(object):
-    def get_attributes(self):
-        pass
+
+    def get_attributes(self) -> List[Tuple[str, object]]:
+        return []
